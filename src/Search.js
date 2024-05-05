84674a50bd1d1b3d89d7a73b68f3c28d1d44764a
Alessandra198 <rinaldoalessandra@gmail.com>
2024-05-05 18:21:43 +0200

    Called axios

--- src/Search.js
+++ src/Search.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Search() {
+  let [value, setValue] = useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setValue(event.target.value);
+    callAxios();
+  }
+
+  function handleResponse(response) {
+    console.log(response);
+  }
+
+  function callAxios() {
+    const apiKey = "3Q8jVZT959Smky3PiaFSzMegTxXQxyfBbbBQKrhG";
+    let apiUrl = `https://images-api.nasa.gov/search?q=${value}`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
   return (
     <div>
       <h3>What are you looking for?</h3>
-<form>
-    <input type="search" placeholder="Ex. Moon, Apollo 11" />
-    <input type="submit" value="Submit" />
-</form>
+      <form onSubmit={handleSubmit}>
+        <input type="search" placeholder="Ex. Moon, Apollo 11" />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
