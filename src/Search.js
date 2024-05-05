be009092a13965b868fd56db9b1f88a6834fb8ed
Alessandra198 <rinaldoalessandra@gmail.com>
2024-05-05 18:15:27 +0200

    Added form

--- src/Search.js
+++ src/Search.js
@@ -4,6 +4,10 @@ export default function Search() {
   return (
     <div>
       <h3>What are you looking for?</h3>
+<form>
+    <input type="search" placeholder="Ex. Moon, Apollo 11" />
+    <input type="submit" value="Submit" />
+</form>
     </div>
   );
 }
