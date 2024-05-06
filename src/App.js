44e8a15a24e2db8d6e0baf75a6785ae3d99c0a67
Alessandra198 <rinaldoalessandra@gmail.com>
2024-05-06 14:33:40 +0200

    Deleted unecessary lines of code

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Nasa</h1>
-      <h2>Images and Videos Library</h2>
-      <Search />
+      <div className="container">
+        <h1>Nasa</h1>
+        <h2>Images and Videos Library</h2>
+        <Search />
+      </div>
     </div>
   );
 }
